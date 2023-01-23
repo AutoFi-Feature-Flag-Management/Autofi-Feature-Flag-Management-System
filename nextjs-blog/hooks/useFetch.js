85b9d57c0b10c996bf7755861868a686ceba8b9c
IWwilliams <isaiah.williams@ucalgary.ca>
2023-01-22 20:37:10 -0700

    Merges completed toggle component

--- nextjs-blog/hooks/useFetch.js
+++ nextjs-blog/hooks/useFetch.js
@@ -1,8 +1,25 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 function useFetch() {
   const [isLoading, setIsLoading] = useState(false);
-
-  return null;
+  const [error, setError] = useState(false);
+  const fetchData = useCallback(async (fetchCallInfo, applyData) => {
+    setIsLoading(true);
+    setError(false);
+    try {
+      const response = fetch(fetchCallInfo.url, {
+        method: fetchCallInfo.method ? fetchCallInfo.method : "GET",
+        headers: fetchCallInfo.headers ? fetchCallInfo.headers : {},
+        body: fetchCallInfo.body ? JSON.stringify(fetchCallInfo.body) : null,
+      });
+      if (!response.ok) {
+        throw new Error("Request failed please review console log.");
+      }
+    } catch {
+      setError(err.message || "Something went wrong");
+    }
+    setIsLoading(false);
+  }, []);
+  return { error, isLoading, sendRequest };
 }
 export default useFetch();
