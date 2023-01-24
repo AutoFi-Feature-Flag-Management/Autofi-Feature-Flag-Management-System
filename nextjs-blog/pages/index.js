bf156fb64d0ffd6175cc4daaab91344301f498d8
tysontrail <tyson.trail@gmail.com>
2023-01-23 19:55:11 -0700

    Reverted Index.js

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import Toggle from "../components/Toggle";
-import Button from "../components/Button";
-
 function HomePage() {
-  const onButtonClick = () => {
-    console.log("Button was clicked");
-  };
-
   return (
     <React.Fragment>
       <h1>The home Page</h1>
       <Toggle></Toggle>
-      <Button onClick={onButtonClick} title="Test"></Button>
     </React.Fragment>
   );
 }
