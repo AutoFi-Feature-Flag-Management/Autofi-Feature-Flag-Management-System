66fd95c6cf95ca209e28a706995dacb6975195ba
Tyson Trail <62910152+tysontrail@users.noreply.github.com>
2023-02-15 17:10:23 -0700

    Combines separate launch darkly single and multi feature flag marshalling into a single function

--- backend/app/businessLayer/marshals/launchDarklyFeatureFlagMarshal.js
+++ backend/app/businessLayer/marshals/launchDarklyFeatureFlagMarshal.js
@@ -7,55 +7,47 @@
 const FeatureFlag = require("../../../../shared/model/featureFlag");
 
 /**
- * Marshals an array of feature flag data from a LaunchDarkly JSON file.
+ * Marshals feature flag data into an array from a LaunchDarkly JSON file.
  *
  * @param {string} json_file - The LaunchDarkly JSON file.
- * @returns {Array} An array of marshalled feature flags.
+ * @returns {Array} An array of marshalled feature flags (or single flag).
  */
-launchDarklyFlagsMarshaller = (json_file) => {
+launchDarklyFlagMarshaller = (json_file) => {
   //parsifying the launch darkly json_file into an object
   const launch_darkly_json = JSON.parse(json_file);
 
   //creating feature flag array
   let feature_flags = [];
 
-  launch_darkly_json.items.forEach((item) => {
-    feature_flags.push(
-      new FeatureFlag(
-        item.key,
-        item.name,
-        item.environments.production.on,
-        new Date(item.environments.production.lastModified),
-        item.description
-      )
+  if (Array.isArray(launch_darkly_json.items)) {
+    // If request is multiple feature flags
+    launch_darkly_json.items.forEach((item) => {
+      feature_flags.push(
+        new FeatureFlag(
+          item.key,
+          item.name,
+          item.environments.production.on,
+          new Date(item.environments.production.lastModified),
+          item.description
+        )
+      );
+    });
+  } else {
+    // If request is a single feature flag
+    const feature_flag = new FeatureFlag(
+      launch_darkly_json.key,
+      launch_darkly_json.name,
+      launch_darkly_json.environments.production.on,
+      new Date(launch_darkly_json.environments.production.lastModified),
+      launch_darkly_json.description
     );
-  });
+
+    feature_flags.push(feature_flag);
+  }
 
   return feature_flags;
 };
 
-/**
- * Marshals a single feature flag data from a LaunchDarkly JSON file.
- *
- * @param {string} json_file - The LaunchDarkly JSON file.
- * @returns {Object} A feature flag object from the marshalled feature flag.
- */
-launchDarklyFlagMarshaller = (json_file) => {
-  //parsifying the launch darkly json_file into an object
-  const launch_darkly_json = JSON.parse(json_file);
-
-  const feature_flag = new FeatureFlag(
-    launch_darkly_json.key,
-    launch_darkly_json.name,
-    launch_darkly_json.environments.production.on,
-    new Date(launch_darkly_json.environments.production.lastModified),
-    launch_darkly_json.description
-  );
-
-  return feature_flag;
-};
-
 module.exports = {
   launchDarklyFlagMarshaller,
-  launchDarklyFlagsMarshaller,
 };
