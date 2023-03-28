48e8a8e597245afec732a7f9446b4fa2a9d9d846
Your Name <kenneth.weech@gmail.com>
2023-03-27 18:39:14 -0600

    fixed typo

--- backend/app/routes/featureFlags.js
+++ backend/app/routes/featureFlags.js
@@ -3,7 +3,7 @@
  */
 
 const express = require("express");
-const clientController = require("../clientContoller/clientController");
+const clientController = require("../clientController/clientController");
 const router = express.Router();
 
 /**
