173049927f76f368115281988338d084293508d0
KBaldwin2 <kbaldwin@aeriumanalytics.com>
2023-04-12 10:20:35 -0600

    added js docs

--- clientUI/components/UI/Header.js
+++ clientUI/components/UI/Header.js
@@ -2,7 +2,16 @@ import logo from "../../public/AutoFi_Logo_2020.png";
 import Image from "next/Image";
 import classes from '../../styles/Header.module.css';
 
-const Header = (props) => {
+/**
+
+This is the Header component of the AutoFi website.
+It displays the company logo in the header section.
+@returns {JSX.Element} - A JSX element representing the Header component.
+@example
+<Header />
+*/
+
+const Header = () => {
     return(
         <div className={classes.header}><Image src={logo} className={classes.img}></Image>
         </div>
